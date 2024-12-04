943c6a4e2222014f79eb52f4659e4e8c20ff5baa
Prathamesh Patil <prathameshp@thoughtsketch.com>
2024-12-04 16:30:47 +0530

    added_bottom_navigationbar

---
+++ lib/screens/landing_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:ts_social/screens/feature1_screen.dart';
+import 'package:ts_social/screens/feature2_screen.dart';
+import 'package:ts_social/screens/feature3_screen.dart';
+import 'package:ts_social/widgets/app_drawer.dart';
+
+class LandingScreen extends StatefulWidget {
+  const LandingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LandingScreen> createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  int _selectedIndex = 0;
+  final PageController _pageController = PageController();
+
+  final List<Widget> _screens = [
+    const Feature1Screen(),
+    const Feature2Screen(),
+    const Feature3Screen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    // Slide to the selected screen using PageController
+    _pageController.jumpToPage(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: const AppDrawer(),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        children: _screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:ts_social/constants/styles.dart';
 import 'package:ts_social/screens/create_account_screen.dart';
-import 'package:ts_social/screens/home_screen.dart';
+import 'package:ts_social/screens/landing_screen.dart';
 import 'package:ts_social/widgets/custom_button.dart';
 import 'package:ts_social/widgets/password_input_field.dart';
 import 'package:ts_social/widgets/text_input_field.dart';
@@ -31,7 +31,7 @@ class _LoginScreenState extends State<LoginScreen> {
           password: _passwordController.text.trim(),
         );
         if (result.isSignedIn) {
-          Navigator.push(context, MaterialPageRoute(builder: (context)=>const HomeScreen(),),);
+          Navigator.push(context, MaterialPageRoute(builder: (context)=>const LandingScreen(),),);
         } else {
           setState(() => _errorMessage = "Sign-in not complete");
         }
